e9936a8759e5408ff24d0c2a9443903b74315e54
Ricardo Gaya <rrgaya@gmail.com>
2019-06-17 17:01:54 -0300

    Edit Pedido

--- vendas/urls.py
+++ vendas/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import DashboardView, Novo_pedido, NovoItemPedido
+from .views import DashboardView, Novo_pedido, NovoItemPedido, ListaVendas,EditPedido
 
 
 urlpatterns = [
+    path('', ListaVendas.as_view(), name='lista_vendas'),
     path('novo-pedido/', Novo_pedido.as_view(), name='novo_pedido'),
     path('dashboard/', DashboardView.as_view(), name='dashboard'),
     path('novo-item-pedido/<int:venda>', NovoItemPedido.as_view(), name='novo-item-pedido'),
+    path('edit-pedido/<int:venda>', EditPedido.as_view(), name='edit_pedido'),
 ]
