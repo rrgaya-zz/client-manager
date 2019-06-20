67d91984c1680ce0d0333083273ae320fac0b2b3
Ricardo Gaya <rrgaya@gmail.com>
2019-06-20 08:41:46 -0300

    URL UPDATE

--- api/urls.py
+++ api/urls.py
@@ -4,7 +4,7 @@ from .views import ArticleView, PostList, PostDetail
 
 app_name = "articles"
 
-# app_name will help us do a reverse look-up latter.
+
 urlpatterns = [
     path('', PostList.as_view()),
     path('<int:pk>', PostDetail.as_view()),
