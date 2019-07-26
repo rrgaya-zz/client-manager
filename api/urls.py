f904c8cfde2025d7e4475dcc7c2fd037913e2277
Ricardo Gaya <rrgaya@gmail.com>
2019-07-26 01:48:51 -0300

    Add api

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,13 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+from api import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
 
 urlpatterns = [
-    # path('',),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
