0e7ee00455fad0b01949c522192f212ed8f2d11a
Ricardo Gaya <rrgaya@gmail.com>
2019-08-03 00:14:53 -0300

    function product_list simples

--- produtos/urls.py
+++ produtos/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ProdutoApi
+from .views import ProdutoApi, product_list
 
 
 urlpatterns = [
+    path('', product_list, name='product_list'),
     path('api/', ProdutoApi.as_view(), name='produto_apu'),
 ]
--- produtos/views.py
+++ produtos/views.py
@@ -1,8 +1,14 @@
 from .models import Produto
 from .serializers import ProdutoSerializer
 from rest_framework import generics
+from django.shortcuts import render
 
 
 class ProdutoApi(generics.ListAPIView):
     queryset = Produto.objects.all()
     serializer_class = ProdutoSerializer
+
+
+def product_list(request):
+    produtos = Produto.objects.all()
+    return render(request, "produtos/produtos.html", {"produtos": produtos})
