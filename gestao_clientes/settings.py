f804eaad9394ecab2982994343e2556c7fb676be
rrgaya <rrgaya@gmail.com>
2019-09-24 22:16:41 -0300

    Add docker-composer

--- gestao_clientes/settings.py
+++ gestao_clientes/settings.py
@@ -92,6 +92,18 @@ DATABASES = {
 }
 
 
+# DATABASES = {
+#     'default': {
+#     'ENGINE': config('DB_ENGINE'),
+#     'NAME': config('DB_NAME'),
+#     'USER': config('DB_USER'),
+#     'PASSWORD': config('DB_PASSWORD'),
+#     'HOST': config('DB_HOST'),
+#     'PORT': config('DB_PORT'),
+#   }
+# }
+
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
